9c605d47272484efdfeb6c327ecd6d9af2911b97
Pataatom <pataatom@gmail.com>
2024-10-19 21:26:06 +0200

    created main.py and made basic alarm siren

---
+++ Camera/main.py
@@ -0,0 +1,5 @@
+import playsound
+def alarm_sound(alarm_path):
+    playsound.playsound(alarm_path)
+
+alarm_sound(r"C:\Users\User\Desktop\skola\Alba\Morčata na útěku - Lída v akci\Morčata na útěku - Děda se našel #nenavazuje [F4__OZHvDv4].mp3")
