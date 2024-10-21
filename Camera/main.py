a532982288431cf7bd2b3c53140859c4766b2bfc
Pataatom <pataatom@gmail.com>
2024-10-21 20:30:46 +0200

    Added basic alarm siren function to main.py

--- Camera/main.py
+++ Camera/main.py
@@ -1,5 +1,17 @@
-import playsound
-def alarm_sound(alarm_path):
-    playsound.playsound(alarm_path)
+import pygame
+import time
 
-alarm_sound(r"C:\Users\User\Desktop\skola\Alba\Morčata na útěku - Lída v akci\Morčata na útěku - Děda se našel #nenavazuje [F4__OZHvDv4].mp3")
+pygame.mixer.init()
+
+
+def alarm_sound(alarm_path):
+    pygame.mixer.music.load(alarm_path)
+    try:
+        pygame.mixer.music.play()
+        while pygame.mixer.music.get_busy():
+            time.sleep(0.5)  # Keep looping while music is playing
+    except KeyboardInterrupt:
+        pygame.mixer.music.stop()
+
+
+# alarm_sound(r"Burglar Alarm Going off with Sirens.mp3")
